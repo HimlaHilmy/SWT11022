d731586bf5ff0624a42cd351fe408c08601b521f
HimlaHilmy <himlahilmy@gmail.com>
2025-05-08 09:06:36 +0530

    Add files via upload

---
+++ StudentRegNo_info.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+  printf ("Welcome To The Student Program!\n");
+  
+
+ 
+  printf("Student Name: Himla \n");
+  printf("Student Registration Number: SEU/IS/22/ICT/080 \n");
+  return 0;
+
+} 
